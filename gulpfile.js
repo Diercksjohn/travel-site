78e8f8a438fee04f871534097b46d4d613c8ba92
John <jdpivotjohn@gmail.com>
2018-02-14 01:58:23 -0600

    added stylesheet included base and module styles and added simple-vars nested and import to gitfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssVars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function(){
 	console.log("Hooray - you created a gulp task")
@@ -10,7 +15,10 @@ gulp.task('html', function(){
 })
 
 gulp.task('styles', function(){
-	console.log("Imagine sass or post css tasks");
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+		
 })
 
 gulp.task('watch', function(){
